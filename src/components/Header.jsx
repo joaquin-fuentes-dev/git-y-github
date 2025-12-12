6001a2b814d345f4b85a527f83748a6cd320c2d5
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-12-12 19:04:37 -0300

    feat: actualizar branding y agregar logo al navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,15 @@ export default function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
       <Container>
-        <Navbar.Brand href="#home">Mi Sitio</Navbar.Brand>
+        <Navbar.Brand href="#home" className="d-flex align-items-center gap-2">
+          <img
+            src="https://img.freepik.com/vector-premium/ilustracion-vectorial-icono-navegador-web-vectorial_921039-10899.jpg?semt=ais_hybrid&w=740&q=80https://img.freepik.com/vector-premium/ilustracion-vectorial-icono-navegador-web-vectorial_921039-10899.jpg?semt=ais_hybrid&w=740&q=80"
+            alt="Logo"
+            className="rounded-circle"
+            style={{ width: "40px", height: "40px", objectFit: "cover" }}
+          />
+          Mi Sitio PRO
+        </Navbar.Brand>{" "}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
