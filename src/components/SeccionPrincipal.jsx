fed33e55595be9ae2d9cc841f25e00e5a7e0fdeb
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-12-12 19:43:51 -0300

    feat: actualizar texto desde dev

--- src/components/SeccionPrincipal.jsx
+++ src/components/SeccionPrincipal.jsx
@@ -4,7 +4,7 @@ export default function SeccionPrincipal() {
   return (
     <Container className="py-5">
       <h2 className="mb-3">Sección Principal</h2>
-      <p>Este es un contenido de prueba dentro de la sección principal.</p>
+      <p> Contenido actualizado desde la rama dev con otra descripción.</p>
     </Container>
   );
 }
