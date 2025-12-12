618d8ddb865cc4fb16b7728b864dcbe79886c057
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-12-12 17:08:31 -0300

    feat: agregá seccion principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
+import SeccionPrincipal from "./components/SeccionPrincipal.jsx";
 
 export default function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main className="flex-fill"></main>
+      <main className="flex-fill">
+        <SeccionPrincipal />
+      </main>
       <Footer />
     </div>
   );
