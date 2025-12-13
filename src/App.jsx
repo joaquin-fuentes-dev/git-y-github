0b4d49f9510ac33e5eba87b3be0827769b322195
Joaquin Fuentes <joaquin.fuentes@rollingcodestudio.com>
2025-12-13 18:53:49 -0300

    feat: modificá color de fondo de main de blanco a amarillo

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Header />
-      <main className="flex-fill">
+      <main className="flex-fill bg-warning">
         <SeccionPrincipal />
       </main>
       <Footer />
